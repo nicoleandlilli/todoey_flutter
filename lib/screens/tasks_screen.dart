e4a03a3e92ae5d3b3674408107f704f1bd5313cf
wangguiye <wangguiye@lenovo.com>
2024-03-29 21:18:34 +0800

    自定义FloatingActionButton

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -8,43 +8,58 @@ class TaskScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child:Icon(Icons.list,size: 30.0,),
-              backgroundColor: Colors.white,
-              radius: 30.0,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {  },
+        child: Icon(Icons.add),
+        shape: CircleBorder(),
+        foregroundColor: Color(0xFFFFFFFF),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child:Icon(Icons.list,size: 30.0,),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Today',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+
+              ],
             ),
-            SizedBox(
-              height: 10.0,
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0),topRight: Radius.circular(20.0)),
             ),
-            Text(
-              'Today',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Expanded(child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0),topRight: Radius.circular(20.0)),
-              ),
-            )),
-          ],
-        ),
+          )),
+        ],
+
+
       ),
     );
   }

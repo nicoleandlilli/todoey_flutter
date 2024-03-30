affbc75f855e7d4dac74e3c18da48ea3ff8fd5fd
wangguiye <wangguiye@lenovo.com>
2024-03-30 19:03:12 +0800

    给文字添加划掉的横线

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+// class TaskTile extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       title: Text(
+//           'This is a task'
+//       ),
+//       trailing: TaskCheckbox(),
+//     );
+//   }
+// }
+
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return TaskTileState();
+  }
+}
+
+
+class TaskTileState extends State {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-          'This is a task'
+          'This is a task',
+              style: TextStyle(
+                decoration: TextDecoration.lineThrough,
+              ),
       ),
-      trailing: Checkbox(
-        value: false, onChanged: (bool? value) {  },
-      ),
+      trailing: TaskCheckbox(isChecked),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget{
+
+  final bool checkboxState;
+
+  TaskCheckbox(this.checkboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (bool? newValue) {
+        // setState(() {
+        //   isChecked = newValue!;
+        // });
+      },
     );
   }
 }
